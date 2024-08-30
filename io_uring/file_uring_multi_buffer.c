122fb3133b89416b6f57d0f2f4092b3ee1e7a485
sunder3344 <sunder3344@users.noreply.github.com>
2024-08-30 16:29:16 +0800

    Add files via upload

--- io_uring/file_uring_multi_buffer.c
+++ io_uring/file_uring_multi_buffer.c
@@ -15,7 +15,7 @@ int dest_fd, source_fd = 0;
 
 struct file_data {
 	off_t offset;
-	char buffer[BUF_SIZE+1];
+	struct iovec iov;
 };
 
 off_t get_file_size(int fd) {
@@ -68,7 +68,7 @@ void read_cqe_recv(struct io_uring *ring, unsigned long count) {
 
 		struct io_uring_sqe *sqe = io_uring_get_sqe(ring);
 		int bytes_read = cqe->res;
-		io_uring_prep_write(sqe, dest_fd, data->buffer, bytes_read, data->offset);
+		io_uring_prep_writev(sqe, dest_fd, &data->iov, 1, data->offset);
 		io_uring_sqe_set_data(sqe, data);
 
 		io_uring_cqe_seen(ring, cqe);
@@ -131,7 +131,6 @@ int main(int argc, char *argv[]) {
 	}
 
 	off_t file_sz = get_file_size(source_fd);
-	//printf("size:= %lu", file_sz);
 	off_t offset = 0;
 	off_t insize = file_sz;
 	int ret, ret2, j;
@@ -141,6 +140,7 @@ int main(int argc, char *argv[]) {
 	int flag = 1;		//1£ºread£º2£ºwrite
 
 	off_t this_size;
+	int depth = 0;
 	while (1) {
 		this_size = BUF_SIZE;
 		if (this_size > insize) {
@@ -148,13 +148,18 @@ int main(int argc, char *argv[]) {
 		}
 		
 		if (flag == 1 && insize > 0) {
-			struct file_data *data = malloc(sizeof(struct file_data));
+			struct file_data *data = malloc(this_size + sizeof(struct file_data));
 			struct io_uring_sqe *sqe;
 			sqe = io_uring_get_sqe(&ring);
+
 			data->offset = offset;
-			io_uring_prep_read(sqe, source_fd, data->buffer, this_size, offset);
+			data->iov.iov_base = data + 1;
+			data->iov.iov_len = this_size;
+
+			io_uring_prep_readv(sqe, source_fd, &data->iov, 1, offset);
 			io_uring_sqe_set_data(sqe, data);
 			reads++;
+			depth++;
 			
 			if ((this_size == insize) || reads >= DEPTH) {		//where queue count equals DEPTH, then submit and read queue
 				ret = io_uring_submit(&ring);
@@ -165,7 +170,7 @@ int main(int argc, char *argv[]) {
 				flag = 2;
 				writes = reads;
 				reads = 0;
-				//printf("submit ret = %d\n", ret);
+				depth = 0;
 				read_cqe_recv(&ring, writes);
 			}
 			insize -= this_size;
